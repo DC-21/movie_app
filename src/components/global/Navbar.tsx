ad103a13fc3f9dc3cc38c95e9b35624357cd451c
dc-21 <cholahkuboko@gmail..com>
2024-01-23 21:45:56 +0200

    styles

--- src/components/global/Navbar.tsx
+++ src/components/global/Navbar.tsx
@@ -1,14 +1,19 @@
 import { Link } from "react-router-dom";
+import * as styles from "../../styles/navbar";
 
 const Navbar = () => {
   return (
-    <div className="w-full flex justify-between z-40 mx-auto p-6 items-center bg-black backdrop:blur-xl sticky text-white top-0 bg-opacity-90">
-      <Link to="/" className="text-xl">
+    <div className={styles.navbarContainer}>
+      <Link to="/" className={styles.logoLink}>
         Movie App
       </Link>
-      <div className="flex gap-4">
-        <Link to="/">Home</Link>
-        <Link to="/liked">Liked</Link>
+      <div className={styles.navLinksContainer}>
+        <Link to="/" className={styles.navLink}>
+          Home
+        </Link>
+        <Link to="/liked" className={styles.navLink}>
+          Liked
+        </Link>
       </div>
     </div>
   );
---
+++ src/styles/navbar.ts
@@ -0,0 +1,5 @@
+export const navbarContainer =
+  "w-full flex justify-between z-40 mx-auto p-6 items-center bg-black backdrop:blur-xl sticky text-white top-0 bg-opacity-90";
+export const logoLink = "text-xl";
+export const navLinksContainer = "flex gap-4";
+export const navLink = "";
